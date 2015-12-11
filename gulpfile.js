86568d31bf061588eaa7141cdc1a4ef7084aa0dc
nan.ji <nan.ji@sim.com>
2015-12-11 14:00:59 +0800

    last submit

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var sass = require('gulp-sass');
 gulp.task('sass', function () {
   gulp.src('./scss/**/*.scss')
     .pipe(sass({outputStyle: 'nested'}).on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('E:/svn/utouu-menke/utouu-menke-web/src/main/webapp/public/css'));
 });
  
 gulp.task('sass:watch', function () {
